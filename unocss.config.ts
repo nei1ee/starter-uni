cd6f211aee1ea5ea60aac7e5bd600cd192d68ded
Neil <zguolee@foxmail.com>
2022-09-22 21:42:51 +0800

    chore: lint

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,37 +1,36 @@
-import type { Preset, SourceCodeTransformer } from "unocss";
 import {
   defineConfig,
   presetAttributify,
   presetIcons,
   transformerDirectives,
   transformerVariantGroup,
-} from "unocss";
+} from 'unocss'
 
 import {
   presetApplet,
   presetRemToRpx,
   transformerApplet,
   transformerAttributify,
-} from "unocss-applet";
+} from 'unocss-applet'
 
-const isH5 = process.env.UNI_PLATFORM === "h5";
+const isH5 = process.env.UNI_PLATFORM === 'h5'
 
 export default defineConfig({
   shortcuts: {
-    "bg-base": "bg-gray-100 dark:bg-dark",
-    "bg-base-second": "bg-white dark:bg-dark-100",
-    "color-base": "text-gray-700 dark:text-light-2",
-    "color-base-second": "text-gray-400 dark:text-gray-500/60",
-    "border-base": "border border-gray-200 dark:border-gray/60",
-    "bg-primary": "bg-light-blue-500 dark:bg-light-blue-600",
+    'bg-base': 'bg-gray-100 dark:bg-dark',
+    'bg-base-second': 'bg-white dark:bg-dark-100',
+    'color-base': 'text-gray-700 dark:text-light-2',
+    'color-base-second': 'text-gray-400 dark:text-gray-500/60',
+    'border-base': 'border border-gray-200 dark:border-gray/60',
+    'bg-primary': 'bg-light-blue-500 dark:bg-light-blue-600',
   },
   presets: [
     presetIcons({
       scale: 1.2,
       warn: true,
       extraProperties: {
-        display: "inline-block",
-        "vertical-align": "middle",
+        'display': 'inline-block',
+        'vertical-align': 'middle',
       },
     }),
     /**
@@ -51,13 +50,13 @@ export default defineConfig({
   ],
   rules: [
     [
-      "p-safe",
+      'p-safe',
       {
         padding:
-          "env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)",
+          'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)',
       },
     ],
-    ["pt-safe", { "padding-top": "env(safe-area-inset-top)" }],
-    ["pb-safe", { "padding-bottom": "env(safe-area-inset-bottom)" }],
+    ['pt-safe', { 'padding-top': 'env(safe-area-inset-top)' }],
+    ['pb-safe', { 'padding-bottom': 'env(safe-area-inset-bottom)' }],
   ],
-});
+})
